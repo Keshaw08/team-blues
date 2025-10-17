6bfecbd7d4f3e329323b7e41a5d4219c4bf01f3c
Keshaw08 <keshawsoni08@gmail.com>
2025-10-17 17:43:01 +0530

    added top scroller

--- front-end/src/pages/About.tsx
+++ front-end/src/pages/About.tsx
@@ -1,9 +1,14 @@
 import AboutUs from "@/components/AboutUs";
 import Header from "@/components/Header";
+import { useEffect } from "react";
 
 type Props = {};
 
 const About = ({}: Props) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <Header />
--- front-end/src/pages/Landing.tsx
+++ front-end/src/pages/Landing.tsx
@@ -5,10 +5,15 @@ import MarathonDetails from "@/components/MarathonDetails";
 import News from "@/components/News";
 import Gallery from "@/components/Gallery";
 import Footer from "@/components/Footer";
+import { useEffect } from "react";
 
 type Props = {};
 
 function Landing({}: Props) {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <Header />
